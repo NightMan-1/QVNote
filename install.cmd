91ee42763b038c972863732a5fa5b1414f344ab7
Sergey Gurinovich <sergey@fsky.info>
2019-03-21 22:03:03 +0300

    update

--- install.cmd
+++ install.cmd
@@ -6,14 +6,18 @@ go get -u github.com/imroc/req
 go get -u github.com/json-iterator/go
 go get -u github.com/kataras/iris
 go get -u github.com/iris-contrib/middleware/cors
-go get -u github.com/satori/go.uuid
-go get -u github.com/siddontang/ledisdb
+go get -u github.com/gofrs/uuid
+go get -u github.com/siddontang/ledisdb/config
+go get -u github.com/siddontang/ledisdb/ledis
+go get -u github.com/json-iterator/go
+go get -u github.com/marcsauter/single
+go get -u github.com/josephspurrier/goversioninfo/cmd/goversioninfo
+go get -u github.com/syndtr/goleveldb/leveldb
+go get -u github.com/go-ini/ini
+
 go get -u github.com/gen2brain/beeep
 go get -u github.com/gen2brain/dlgs
 go get -u github.com/getlantern/systray
 go get -u github.com/gonutz/w32
-go get -u github.com/json-iterator/go
-go get -u github.com/marcsauter/single
-go get -u github.com/josephspurrier/goversioninfo/cmd/goversioninfo
 
 npm install
