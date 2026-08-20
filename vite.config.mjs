9e4b270edc14b6e703a6459d1835d6cbc8c5a803
NightMan-1 <sergey@fsky.info>
2026-08-21 00:02:12 +0300

    Rename vite.config.js to .mjs (native ESM), use import.meta.dirname

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     plugins: [vue()],
     resolve: {
         alias: {
-            '@': resolve(__dirname, 'src')
+            '@': resolve(import.meta.dirname, 'src')
         }
     },
     server: {
